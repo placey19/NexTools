4fafa108de3f7cbe636bbcede3fcfc01c35bf873
davidplace1 <david.t.place@gmail.com>
2025-05-18 00:58:25 +0100

    Added EnableInSceneView option to ColorFormatFeature

--- PostProcessing/Scripts/Runtime/Misc/ColorFormatFeature.cs
+++ PostProcessing/Scripts/Runtime/Misc/ColorFormatFeature.cs
@@ -5,6 +5,7 @@ public class ColorFormatFeature : ScriptableRendererFeature {
 
     public RenderPassEvent When = RenderPassEvent.BeforeRenderingPostProcessing;
     public GraphicsFormat ColorFormat = GraphicsFormat.B5G5R5A1_UNormPack16;
+    public bool EnableInSceneView = true;
 
     private ColorFormatPass _pass;
 
@@ -15,6 +16,10 @@ public class ColorFormatFeature : ScriptableRendererFeature {
     }
 
     public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+        if (renderingData.cameraData.isSceneViewCamera && !EnableInSceneView) {
+            return;
+        }
+
         renderer.EnqueuePass(_pass);
     }
 }
