ae937915772e10b9c15d5425b8c6cf9c983e9729
davidplace1 <david.t.place@gmail.com>
2025-05-17 20:15:14 +0100

    Added Misc/ColorFormatFeature & Pass

---
+++ PostProcessing/Scripts/Runtime/Misc/ColorFormatFeature.cs
@@ -0,0 +1,20 @@
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ColorFormatFeature : ScriptableRendererFeature {
+
+    public RenderPassEvent When = RenderPassEvent.BeforeRenderingPostProcessing;
+    public GraphicsFormat ColorFormat = GraphicsFormat.B5G5R5A1_UNormPack16;
+
+    private ColorFormatPass _pass;
+
+    public override void Create() {
+        _pass = new ColorFormatPass(ColorFormat) {
+            renderPassEvent = When
+        };
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+        renderer.EnqueuePass(_pass);
+    }
+}
